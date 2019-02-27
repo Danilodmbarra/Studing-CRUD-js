34d66318b5039b2d3106b789e01d104b9b3f2ab8
Daniel Barra <dmbarra@gmail.com>
2019-02-27 13:46:58 -0300

    CRUD finalizado part1

--- server.js
+++ server.js
@@ -1,17 +1,80 @@
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true}))
+//Chamada dos pacotes:
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId
 
-app.listen(3000,function(){
-  console.log('Server esta funcionando!')
-})
-app.set('view engine','ejs')
-app.get ('/',(req,res) => {
-  res.render('index.ejs')
-app.post('/show',(req,res)=> {
-  console.log(req.body)
-})
+//config do Body-parser para usar json:
+app.use(bodyParser.urlencoded({ extended: true}));
 
+
+//Conexao com banco de dados e definindo porta:
+
+const uri = "mongodb+srv://danilodmbarra:123danilo@crudm-xwvck.mongodb.net/dataretryWrites=true";
+MongoClient.connect(uri,(err,client) => {
+    if(err) return console.log(err)
+    db = client.db('data')
+    app.listen(3000, () =>{
+            console.log('Server esta funcionando!')
+    })
+});
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+//
+
+app.get('/',(req,res) =>{
+ res.render('index.ejs')
+})
+app.post('/show',(req,res) => {
+  db.collection('carro').save(req.body,(err,result)=>{
+    if (err) return console.log(err)
+    res.redirect('/show')
+      })
+})
+app.get('/show',(req,res)=>{
+  db.collection('carro').find().toArray((err,results)=>{
+    res.render('show.ejs',{data : results})
+  })
+})
+app.get('/',(req,res) =>{
+  let cursor = db.collection('carro').find()
+})
+//Editar-
+
+app.route('/edit/:id').get((req, res) => {
+  var id = req.params.id
+
+  db.collection('carro').find(ObjectId(id)).toArray((err, result) => {
+    if (err) return res.send(err)
+    res.render('edit.ejs', { data: result })
+  })
+})
+.post((req, res) => {
+  var id = req.params.id
+  var veiculo = req.body.veiculo
+  var cor = req.body.cor
+
+  db.collection('carro').updateOne({_id: ObjectId(id)}, {
+    $set: {
+      veiculo: veiculo,
+      cor: cor
+    }
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.redirect('/show')
+    console.log('Atualizado no Banco de Dados')
+  })
+})
+app.route('/delete/:id')
+.get((req,res)=>{
+  var id = req.params.id
+  db.collection('carro').deleteOne({_id: ObjectId(id)},(err,result)=>{
+    if (err) return res.send(500,err)
+    console.log('Deletado com sucesso')
+    res.redirect('/show')
+  })
 })
